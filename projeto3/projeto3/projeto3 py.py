0e0f1514469be0d84b0250c517741c785617de4c
Jaqueline <112207859+Jaquelinegb@users.noreply.github.com>
2022-09-17 21:03:37 -0300

    Add files via upload

---
+++ projeto3/projeto3/projeto3 py.py
@@ -0,0 +1,16 @@
+import requests
+import webbrowser
+
+pagina = requests.get('https://g1.globo.com/politica/')
+print(pagina.status_code)
+
+with open('politica.html', 'wb')as arquivo:
+    for texto in pagina.iter_content(1048576):
+        arquivo.write(texto)
+
+"""
+arquivo = open('politica.html', 'wb')
+for texto in pagina.iter_content(1048576):
+    arquivo.write(texto)
+arquivo.close()
+"""
